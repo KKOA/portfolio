08e1cb8502073c473446727977c48c1d117a475b
KEITH-GAMES <ka33145@googlemail.com>
2017-05-28 01:37:02 +0100

    Create quizdb to hold common database task for quizForm, quizFormV2 & quiz_result.php

---
+++ includes/quizdb.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+Description : Retrieve list of question, possible answer and correct answer from database
+Author : Keith Amoah
+Date : 03/11/2016
+*/
+
+//Declare $question array
+$questions =[];
+
+$conn=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$sql="SELECT * FROM quiz ORDER BY question_Id";
+
+if ($result=mysqli_query($conn,$sql))
+{
+
+  while ($row=mysqli_fetch_row($result))// Fetch one and one row
+  {
+      $questions [] = array($row[1],explode(",",$row[2]),$row[3]);// Assign to global array
+  }
+
+  mysqli_free_result($result);// Free result set
+}
+
+mysqli_close($conn);// Close Database Connection
+
+//Number of Questions
+$totalQuestion = count($questions);
