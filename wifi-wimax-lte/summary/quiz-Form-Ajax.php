1b0be40a0b1fa4362255875d4464f8c25874134c
KEITH-GAMES <ka33145@googlemail.com>
2017-06-01 18:40:00 +0100

    Remove redundant code and move result button into buttons

--- wifi-wimax-lte/summary/quiz-Form-Ajax.php
+++ wifi-wimax-lte/summary/quiz-Form-Ajax.php
@@ -3,42 +3,6 @@ include_once("../../includes/config.php");
 require_once("../../includes/quizdb.php");
 ?>
 
-<?php
-/*
-Description : Retrieve list of question, possible answer and correct answer from database
-Author : Keith Amoah
-Date : 03/11/2016
-*/
-/*
-//Declare $question array
-$questions =[];
-
-$conn=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-// Check connection
-if (mysqli_connect_errno())
-{
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-$sql="SELECT * FROM quiz ORDER BY question_Id";
-
-if ($result=mysqli_query($conn,$sql))
-{
-
-  while ($row=mysqli_fetch_row($result))// Fetch one and one row
-  {
-      $questions [] = array($row[1],explode(",",$row[2]),$row[3]);// Assign to global array
-  }
-
-  mysqli_free_result($result);// Free result set
-}
-
-mysqli_close($conn);// Close Database Connection
-
-//Number of Questions
-$totalQuestion = count($questions);
-*/
-?>
 
 <form action="quiz-result.php" method="post" id="quizForm">
 <?php
@@ -81,8 +45,9 @@ for($qid=0;$qid < count($questions);$qid++)
         Click submit to see your result.<br> If want to review your answer select previous.
 
     </div>
-    <input type='submit' name ='submit' id="submit" class='btn btn-default' value='RESULT' >
-
+    <div class="buttons">
+      <input type='submit' name ='submit' id="submit" class='btn btn-default' value='RESULT' >
+    </div>
 </div>
 </form>
 <script src="../../js/question.js"></script>
