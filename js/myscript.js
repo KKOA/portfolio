ae13191d2d0648fac92c3dea3579413842d5d5d0
KEITH-GAMES <ka33145@googlemail.com>
2017-06-04 17:07:43 +0100

    clean up myscript.js with variables

--- js/myscript.js
+++ js/myscript.js
@@ -14,29 +14,28 @@ $(document).ready(function()
        $(".nav > li >a.dropdown-toggle").each(function()
        {
            $this = $(this);
+           childSpan = $this.children('span');
            if($dropdownbtnid == $this.attr("id"))
            {
-
                //Check dropdown menu is open
                if($this.closest("li").hasClass('open')==false)
                 {
-
-                    $this.children('span').removeClass('fa-caret-down');
-                    $this.children('span').addClass('fa-caret-up');
+                  childSpan.removeClass('fa-caret-down');
+                  childSpan.addClass('fa-caret-up');
                 }
                 else
                 {
-                   $this.children('span').removeClass('fa-caret-up');
-                   $this.children('span').addClass('fa-caret-down');
+                  childSpan.children('span').removeClass('fa-caret-up');
+                  childSpan.children('span').addClass('fa-caret-down');
                 }
            }
            else
            {
                 //change caret to point down if up.
-                if($this.children('span').hasClass('fa-caret-up')==true)
+                if(childSpan.hasClass('fa-caret-up')==true)
                 {
-                    $this.children('span').removeClass('fa-caret-up');
-                    $this.children('span').addClass('fa-caret-down');
+                    childSpan.removeClass('fa-caret-up');
+                    childSpan.addClass('fa-caret-down');
                 }
            }
        });
@@ -44,51 +43,38 @@ $(document).ready(function()
 
     function revealToTopLink()
     {
+      bottomlink = $("#bottom > a");
       if ($(this).scrollTop() > 80)
         {
-            $("#bottom > a").css('visibility','visible');
-            $("#bottom > a").fadeIn();
+            bottomlink.css('visibility','visible');
+            bottomlink.fadeIn();
         }
         else
         {
-            $("#bottom > a").fadeOut();
-            $("#bottom > a").css('visibility','hidden');
+            bottomlink.fadeOut();
+            bottomlink.css('visibility','hidden');
         }
     }
 
-
+    $(".no-js").each(
+      function ()
+      {
+        $(this).switchClass("no-js","js",0);
+        // Require JQuery UI
+      }
+    );
 
     //Toggle dropmenu caret icon
     $(".dropdown-toggle").on("click",toggleCaret);
-
 
     //Hide bottom > a link
     $("#bottom > a").css('visibility','hidden');
 
     //Only reveal link when scrolldown 80px down
     $(window).scroll(revealToTopLink);
-    /*$(window).scroll(function() {
-        if ($(this).scrollTop() > 80)
-        {
 
-            $("#bottom > a").css('visibility','visible');
-            $("#bottom > a").fadeIn();
-
-        }
-        else
-        {
-
-            $("#bottom > a").fadeOut();
-            $("#bottom > a").css('visibility','hidden');
-        }
-
-    });*/
 
     // Scroll To Top of page
     $("#bottom > a").click(scrolltoTop);
-    //$("#bottom > a").click(function(event)
-    //{
-        /*event.stopPropagation();
-        $('html,body').animate({scrollTop: 0}, 600);*/
-    //});
+
 });
