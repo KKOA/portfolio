1b507723d4ebbd4e5904255256e44f6b5364df2d
KEITH-GAMES <ka33145@googlemail.com>
2017-06-04 03:43:19 +0100

    Assign questions , possible answers and correct answers to seperate arrays in quizdb.php

--- includes/quizdb.php
+++ includes/quizdb.php
@@ -8,6 +8,8 @@ Date : 03/11/2016
 
 //Declare $question array
 $questions =[];
+$possibleAnswers = [];
+$correctAnswers = [];
 
 $conn=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 // Check connection
@@ -23,7 +25,10 @@ if ($result=mysqli_query($conn,$sql))
 
   while ($row=mysqli_fetch_row($result))// Fetch one and one row
   {
-      $questions [] = array($row[1],explode(",",$row[2]),$row[3]);// Assign to global array
+      //$questions [] = array($row[1],explode(",",$row[2]),$row[3]);// Assign to global array
+      $questions [] = $row[1];
+      $possibleAnswers[] = explode(",",$row[2]);
+      $correctAnswers[] = $row[3];
   }
 
   mysqli_free_result($result);// Free result set
