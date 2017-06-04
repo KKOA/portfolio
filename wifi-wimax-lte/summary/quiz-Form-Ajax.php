8c4467163f88a2026c0e47bcecc42df9fbf86933
KEITH-GAMES <ka33145@googlemail.com>
2017-06-04 10:54:39 +0100

    Change source for questions, possible answer and correct answer and remove unwanted code/files

--- wifi-wimax-lte/summary/quiz-Form-Ajax.php
+++ wifi-wimax-lte/summary/quiz-Form-Ajax.php
@@ -9,32 +9,24 @@ require_once("../../includes/quizdb.php");
 for($qid=0;$qid < count($questions);$qid++)
 {
     echo '<div  id="question'.($qid+1).'" class="questions">';
-    for($j=0;$j < count($questions[$qid]);$j++)
-    {
-        if($j == 0) //Question
-        {
-            $questiondiv = "<div class='question'>";
-            $questiondiv .= "<h2 style='text-align:center;'>Question ".($qid+1)." out of $totalQuestion</h2>";
-            $questiondiv .= "<h3>".$questions[$qid][$j]."</h3>";
-            $questiondiv .= "<input type='hidden' name='question".$qid."_text'  value='".$questions[$qid][$j]."'>";
-            $questiondiv .= "</div>";
-            echo $questiondiv;
-        }
-        elseif($j == 1)
-        {
 
-            $answerdiv ="<div class='answers'>";
-            for($aid=0;$aid < count($questions[$qid][$j]);$aid++)
-            {
-                $answerdiv .= "<input type='radio' name='question".$qid."_answer'  value='".$questions[$qid][$j][$aid]."'>";
-                $answerdiv .=  $questions[$qid][$j][$aid].'<br>';
-            }
-            $answerdiv .= "</div>";
+  $questiondiv = "<div class='question'>";
+  $questiondiv .= "<h2 style='text-align:center;'>Question ".($qid+1)." out of $totalQuestion</h2>";
+  $questiondiv .= "<h3>".$questions[$qid]."</h3>";
+  $questiondiv .= "<input type='hidden' name='question".$qid."_text'  value='".$questions[$qid]."'>";
+  $questiondiv .= "</div>";
+  echo $questiondiv;
+  $NoOfpossibleAnswers = count($possibleAnswers[$qid]);
+  $answerdiv ="<div class='answers'>";
+  for($aid=0;$aid < $NoOfpossibleAnswers;$aid++)
+  {
+    $answerdiv .= "<input type='radio' name='question".$qid."_answer'  value='".$possibleAnswers[$qid][$aid]."'>";
+    $answerdiv .=  $possibleAnswers[$qid][$aid].'<br>';
+  }
+  $answerdiv .= "</div>";
 
-            echo  $answerdiv;
-        }
-     }
-     echo'</div>';
+  echo  $answerdiv;
+  echo'</div>';
 }?>
 <div class="questions" id="check">
     <div class="question">
@@ -50,4 +42,7 @@ for($qid=0;$qid < count($questions);$qid++)
     </div>
 </div>
 </form>
-<script src="../../js/question.js"></script>
+<script type="text/javascript">
+    var questions = <?php echo json_encode($questions); ?>;
+    var correctAnswers = <?php echo json_encode($correctAnswers); ?>;
+</script>
