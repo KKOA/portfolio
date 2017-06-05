881245edb33af0c36dd6aced01b46d0659bea2d9
KEITH-GAMES <ka33145@googlemail.com>
2017-06-05 01:46:48 +0100

    Reference the context of #Box frp, quiz-Form-Ajax.php & clean code in quiz-Form.php

--- wifi-wimax-lte/summary/quiz-Form.php
+++ wifi-wimax-lte/summary/quiz-Form.php
@@ -1,6 +1,5 @@
 <?php
 include_once("../../includes/config.php");
-require_once("../../includes/quizdb.php");
 $title = 'Quiz';
 ?>
 
@@ -21,11 +20,8 @@ $title = 'Quiz';
     <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
     <link rel="stylesheet" href="../../styles/css/mystyle.css">
     <link rel="stylesheet" href="../../styles/css/quiz.css">
-    <style>
-        .questions { display: block;}
-    </style>
 </head>
-<body>
+<body class="no-js">
 <div class="wrapper">
 <?php
     include("../../includes/header.php")
@@ -46,75 +42,13 @@ $title = 'Quiz';
 
                 <p><b>(JavaScript enable user Only)</b><br>
                 Only one question will appear at any time.Use next button and previous button to navigate the quiz.</p>
-               <div id="box" class="shadow">
-                <form action="quiz-result.php" method="post" id="quizForm">
-                <?php
-                for($qid=0;$qid < count($questions);$qid++)
-                {
-                    echo '<div  id="question'.($qid+1).'" class="questions">';
-                    /*for($j=0;$j < count($questions[$qid]);$j++)
-                    {
-                        if($j == 0) //Question
-                        {
-                            $questiondiv = "<div class='question'>";
-                            $questiondiv .= "<h2 style='text-align:center;'>Question ".($qid+1)." out of $totalQuestion</h2>";
-                            $questiondiv .= "<h3>".$questions[$qid][$j]."</h3>";
-                            $questiondiv .= "<input type='hidden' name='question".$qid."_text'  value='".$questions[$qid][$j]."'>";
-                            $questiondiv .= "</div>";
-                            echo $questiondiv;
-                        }
-                        elseif($j == 1)
-                        {
-
-                            $answerdiv ="<div class='answers'>";
-                            for($aid=0;$aid < count($questions[$qid][$j]);$aid++)
-                            {
-                                $answerdiv .= "<input type='radio' name='question".$qid."_answer'  value='".$questions[$qid][$j][$aid]."'>";
-                                $answerdiv .=  $questions[$qid][$j][$aid].'<br>';
-                            }
-                            $answerdiv .= "</div>";
-
-                            echo  $answerdiv;
-                        }
-                    }*/
-                    $questiondiv = "<div class='question'>";
-                    $questiondiv .= "<h2 style='text-align:center;'>Question ".($qid+1)." out of $totalQuestion</h2>";
-                    $questiondiv .= "<h3>".$questions[$qid]."</h3>";
-                    $questiondiv .= "<input type='hidden' name='question".$qid."_text'  value='".$questions[$qid]."'>";
-                    $questiondiv .= "</div>";
-                    echo $questiondiv;
-                    $NoOfpossibleAnswers = count($possibleAnswers[$qid]);
-                    $answerdiv ="<div class='answers'>";
-                    for($aid=0;$aid < $NoOfpossibleAnswers;$aid++)
-                    {
-                    $answerdiv .= "<input type='radio' name='question".$qid."_answer'  value='".$possibleAnswers[$qid][$aid]."'>";
-                    $answerdiv .=  $possibleAnswers[$qid][$aid].'<br>';
-                    }
-                    $answerdiv .= "</div>";
-
-                    echo  $answerdiv;
-                    echo'</div>';
-                }?>
-                <div class="questions" id="check">
-                    <div class="question">
-                            <h1 class="text-center">Quiz complete</h1>
-                    </div>
-                    <div class='answers'>
-
-                        Click submit to see your result.<br> If want to review your answer select previous.
-
-                    </div>
-                    <input type='submit' name ='submit' id="submit" class='btn btn-default' value='RESULT' >
-
+               <div id="box" class="shadow2">
+               <?php require_once("quiz-Form-Ajax.php"); ?>
                 </div>
-                </form>
-                </div>
-                <!-- -->
-                </div>
+            </div>
             <footer class="articleFooter" style="padding:10px 0;">
                 <p>Created by <a href="#">Keith Amoah</a>, May 2011</p>
             </footer>
-
         </article>
         <div id="bottom" class="text-center">
             <a href="#Top" title="Navigate to top of the current page">
@@ -138,8 +72,8 @@ $title = 'Quiz';
     </script>
     <?php /* Load Bootstrap Javascript Library (Require JQuery to Load first) */ ?>
     <script src="../../js/vendors/bootstrap.min.js"></script>
-    <script src="../../js/myscript.js"></script>
     <script src='../../js/vendors/modernizr-custom.js'></script>
+    <script src="../../js/myscript.js"></script>
     <script src='../../js/Another.js'></script>
     <script src="../../js/question.js"></script>
 </body>
